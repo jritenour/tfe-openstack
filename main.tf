92f1b8c3631eb5aef820d604dcdc1b06917e3c7c
Jason Ritenour <jason.ritenour@gmail.com>
2019-05-21 13:33:48 -0400

    updated naming convention

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "openstack" {
 }
 
 resource "openstack_compute_instance_v2" "basic" {
-  name            = "cluster-"{count.index}"
+  name            = "cluster-${count.index}"
   image_name        =  "${var.image_name}"
   flavor_name       = "${var.flavor_name}"
   key_pair        = "${var.key}"
