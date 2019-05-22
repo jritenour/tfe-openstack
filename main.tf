7bc07f1b7bf02829a06d7eeec2001ea842974077
Jason Ritenour <jritenou@redhat.com>
2019-05-21 21:43:48 -0400

    added security group creation

--- main.tf
+++ main.tf
@@ -14,12 +14,36 @@ provider "openstack" {
   region   = "${data.vault_generic_secret.openstack_credentials.data["region"]}"
 }
 
+resource "openstack_compute_secgroup_v2" "secgroup" {
+  name        = "ssh_http"
+  description = "my security group"
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 80
+    to_port     = 80
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
+
 resource "openstack_compute_instance_v2" "basic" {
   name            = "${var.name}-${count.index}"
   image_name        =  "${var.image_name}"
   flavor_name       = "${var.flavor_name}"
   key_pair        = "${var.key}"
-  security_groups = ["default"]
+  security_groups = ["ssh_http"]
+  depends_on = ["openstack_compute_secgroup_v2.secgroup"]
+
+  metadata = {
+    env = "prod"
+  }
 
   network {
     name = "${var.network}"
